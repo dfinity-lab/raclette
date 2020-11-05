1c7a5bc7a1ccfce8c32df8a85ab64370453022be
Roman Kashitsyn <roman@dfinity.org>
2020-11-05 23:28:45 +0100

    report: colorize ok/not ok in TAP output
    This way it's easier to spot failed tests in the output.

--- src/report.rs
+++ src/report.rs
@@ -1,20 +1,45 @@
 use crate::execution::{CompletedTask, Report, Status, Task};
+use term::color::{Color, BRIGHT_GREEN, BRIGHT_RED};
 
 pub struct TapReport {
+    out: Box<term::StdoutTerminal>,
+    out_supports_color: bool,
+    out_supports_reset: bool,
     count: usize,
     total: usize,
 }
 
 impl TapReport {
     pub fn new() -> Self {
-        Self { total: 0, count: 0 }
+        let out = term::stdout().unwrap();
+        let out_supports_color = out.supports_color();
+        let out_supports_reset = out.supports_reset();
+        Self {
+            out,
+            out_supports_color,
+            out_supports_reset,
+            total: 0,
+            count: 0,
+        }
+    }
+
+    fn fg(&mut self, color: Color) {
+        if self.out_supports_color {
+            self.out.fg(color).unwrap();
+        }
+    }
+
+    fn reset(&mut self) {
+        if self.out_supports_reset {
+            self.out.reset().unwrap();
+        }
     }
 }
 
 impl Report for TapReport {
     fn init(&mut self, plan: &[Task]) {
-        println!("TAP version 13");
-        println!("1..{}", plan.len());
+        writeln!(self.out, "TAP version 13").unwrap();
+        writeln!(self.out, "1..{}", plan.len()).unwrap();
         self.total = plan.len();
     }
 
@@ -22,42 +47,50 @@ impl Report for TapReport {
         self.count += 1;
         let ok = task.status == Status::Success;
 
-        println!(
-            "{} {} - {}",
-            if ok { "ok" } else { "not ok" },
-            self.count,
-            task.full_name.join("/")
-        );
+        let (msg, color) = if ok { ("ok", BRIGHT_GREEN) } else { ("not ok", BRIGHT_RED) };
+
+        self.fg(color);
+        write!(self.out, "{} ", msg).unwrap();
+        self.reset();
+
+        writeln!(self.out, "{} - {}", self.count, task.full_name.join("/")).unwrap();
 
         match task.status {
             Status::Success => {
-                println!("# completed in {:?}", task.duration);
+                writeln!(self.out, "# completed in {:?}", task.duration).unwrap();
             }
             Status::Failure(code) => {
-                println!("# process returned {} after {:?}", code, task.duration);
+                writeln!(
+                    self.out,
+                    "# process returned {} after {:?}",
+                    code, task.duration
+                )
+                .unwrap();
             }
             Status::Signaled(signame) => {
-                println!(
+                writeln!(
+                    self.out,
                     "# process was killed with {} after {:?}",
                     signame, task.duration
-                );
+                )
+                .unwrap();
             }
             Status::Timeout => {
-                println!("# timed out after {:?}", task.duration);
+                writeln!(self.out, "# timed out after {:?}", task.duration).unwrap();
             }
         }
 
         if !ok {
             if !task.stdout.is_empty() {
-                println!("# --- stdout ---");
+                writeln!(self.out, "# --- stdout ---").unwrap();
                 for line in task.stdout_as_string().lines() {
-                    println!("# {}", line);
+                    writeln!(self.out, "# {}", line).unwrap();
                 }
             }
             if !task.stderr.is_empty() {
-                println!("# --- stderr ---");
+                writeln!(self.out, "# --- stderr ---").unwrap();
                 for line in task.stderr_as_string().lines() {
-                    println!("# {}", line);
+                    writeln!(self.out, "# {}", line).unwrap();
                 }
             }
         }
